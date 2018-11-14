f00f2e9ba25e3c577b4f75b64d7dc07c90716e73
Bart <bartpetka@gmail.com>
2018-11-14 13:09:02 +0100

    Implement getObject method

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/Client/RemoteClient.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Client/RemoteClient.java
@@ -3,8 +3,11 @@ package remoteObjectsStorage.Client;
 import remoteObjectsStorage.Model.TransferObject;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketOption;
+import java.net.UnknownHostException;
 
 public class RemoteClient {
     private String IP;
@@ -15,12 +18,33 @@ public class RemoteClient {
         this.IP = IP;
     }
 
+
     public void addObject(String key, Object object) throws IOException {
         TransferObject transferObject = new TransferObject(key, object);
+
         Socket socket = new Socket(this.IP, this.socket);
+
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
         objectOutputStream.writeObject(transferObject);
+
         objectOutputStream.flush();
         objectOutputStream.close();
+    }
+
+
+    public Object getObject(String key) throws IOException, ClassNotFoundException {
+        Socket socket = new Socket(this.IP, this.socket);
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject(key);
+        objectOutputStream.flush();
+
+        Object requested = objectInputStream.readObject();
+
+        objectInputStream.close();
+        objectOutputStream.close();
+
+        return requested;
+
     }
 }
