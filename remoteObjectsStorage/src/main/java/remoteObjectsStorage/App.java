20f1f8e1b11901aa34f44753f101f78d201e9253
saturator22 <f.szczeski@gmail.com>
2018-11-14 15:52:58 +0100

    Implement main App

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/App.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/App.java
@@ -1,13 +1,39 @@
 package remoteObjectsStorage;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import remoteObjectsStorage.Client.RemoteClient;
+import remoteObjectsStorage.Server.MTServer;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        String host = scanner.nextLine();
+
+        if(host.equals("server")) {
+            MTServer mtServer = new MTServer(8080);
+            mtServer.run();
+        } else if (host.equals("client")) {
+
+            RemoteClient remoteClient1 = new RemoteClient("localhost", 8080);
+            RemoteClient remoteClient2 = new RemoteClient("localhost", 8080);
+            String string = "POKA POKA";
+
+            try {
+                remoteClient1.addObject("bart", string);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                System.out.println(remoteClient2.getObject("bart"));
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+//        System.out.println(mtServer.getDatabase());
     }
 }
