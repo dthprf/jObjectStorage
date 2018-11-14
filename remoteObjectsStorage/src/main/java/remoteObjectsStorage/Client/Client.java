c45c12bbbb7a40772800fba5d02bc4e30775195e
Bart <bartpetka@gmail.com>
2018-11-14 10:46:34 +0100

    Implement Client class

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/Client/Client.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Client/Client.java
@@ -1,4 +1,26 @@
 package remoteObjectsStorage.Client;
 
+import remoteObjectsStorage.Model.TransferObject;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Client {
+    private String IP;
+    private int socket;
+
+    public Client(String IP, int socket) {
+        this.socket = socket;
+        this.IP = IP;
+    }
+
+    public void addObject(String key, Object object) throws IOException {
+        TransferObject transferObject = new TransferObject(key, object);
+        Socket socket = new Socket(this.IP, this.socket);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject(transferObject);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+    }
 }
