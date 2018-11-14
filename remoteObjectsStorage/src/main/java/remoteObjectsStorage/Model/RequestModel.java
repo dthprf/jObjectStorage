312a74f5099f5c8c9d56acc9a3e032a263ae1f6c
Bart <bartpetka@gmail.com>
2018-11-14 14:02:20 +0100

    Refactor

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/Model/RequestModel.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Model/RequestModel.java
@@ -2,13 +2,15 @@ package remoteObjectsStorage.Model;
 
 import java.io.Serializable;
 
-public class TransferObject implements Serializable {
+public class RequestModel implements Serializable {
     private String key;
     private Object storedObject;
+    private String requestType;
 
-    public TransferObject(String key, Object storedObject) {
+    public RequestModel(String key, Object storedObject, String requestType) {
         this.key = key;
         this.storedObject = storedObject;
+        this.requestType = requestType;
     }
 
     public String getKey() {
@@ -17,5 +19,9 @@ public class TransferObject implements Serializable {
 
     public Object getStoredObject() {
         return storedObject;
+    }
+
+    public String getRequestType() {
+        return requestType;
     }
 }
