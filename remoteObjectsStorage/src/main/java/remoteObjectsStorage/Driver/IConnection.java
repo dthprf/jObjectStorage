71d507148334db64dee9a705c5c65d2fc346f5da
Bart <bartpetka@gmail.com>
2018-11-16 21:10:23 +0100

    Implement connection interface

---
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Driver/IConnection.java
@@ -0,0 +1,12 @@
+package remoteObjectsStorage.Driver;
+
+import remoteObjectsStorage.Model.RequestModel;
+
+import java.io.IOException;
+
+public interface IConnection {
+    void connect(String host, int port);
+    void stopConnection() throws IOException;
+    Object proceedRequest(RequestModel transferObject) throws IOException, ClassNotFoundException;
+    boolean isConnected();
+}
