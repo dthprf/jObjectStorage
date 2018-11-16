21e139390add711569b988a7e8c2e2d5a6ef0fab
Bart <bartpetka@gmail.com>
2018-11-16 21:14:41 +0100

    Refactor Client class to use connection interface

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/Client/RemoteClient.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Client/RemoteClient.java
@@ -1,44 +1,52 @@
 package remoteObjectsStorage.Client;
 
+import remoteObjectsStorage.Constant.MethodType;
+import remoteObjectsStorage.Driver.IConnection;
 import remoteObjectsStorage.Driver.Connection;
+import remoteObjectsStorage.Factory.RequestModelFactory;
 import remoteObjectsStorage.Model.RequestModel;
 
+import java.io.IOException;
+
 public class RemoteClient {
-    private final int port;
-    private final String IP;
+    private String hostAddress;
+    private int port;
+    private Connection clientConnection;
+    private RequestModelFactory requestFactory;
 
-    private final static String getMethod = "GET";
-    private final static String putMethod = "PUT";
-    private final static String deleteMethod = "DELETE";
-
-    public RemoteClient(String address, int port) {
+    public RemoteClient(String hostAddress, int port) {
+        this.hostAddress = hostAddress;
         this.port = port;
-        this.IP = address;
+        clientConnection = new Connection();
+        requestFactory = new RequestModelFactory();
     }
 
     public boolean addObject(String key, Object object) {
-        Connection connection = new Connection(port, IP);
-        RequestModel transferObject = new RequestModel(key, object, putMethod);
-        Object serverResponse = connection.sendRequest(transferObject);
+        RequestModel request = requestFactory.generateRequestModel(MethodType.PUT_METHOD, key, object);
+        Object serverResponse = communicateServer(request);
 
         return (boolean) serverResponse;
     }
 
-
     public Object getObject(String key) {
-        Connection connection = new Connection(port, IP);
-        RequestModel transferObject = new RequestModel(key, null, getMethod);
-        Object serverResponse = connection.sendRequest(transferObject);
+        RequestModel request = requestFactory.generateRequestModel(MethodType.GET_METHOD, key);
+        Object serverResponse = communicateServer(request);
 
         return serverResponse;
-
     }
 
     public boolean removeObject(String key) {
-        Connection connection = new Connection(port, IP);
-        RequestModel transferObject = new RequestModel(key, null, deleteMethod);
-        Object serverResponse = connection.sendRequest(transferObject);
+        RequestModel request = requestFactory.generateRequestModel(MethodType.DELETE_METHOD, key);
+        Object serverResponse = communicateServer(request);
 
         return (boolean) serverResponse;
+    }
+
+    private Object communicateServer(RequestModel request) {
+        clientConnection.connect(this.hostAddress, this.port);
+        Object serverResponse = clientConnection.proceedRequest(request);
+        clientConnection.stopConnection();
+
+        return serverResponse;
     }
 }
