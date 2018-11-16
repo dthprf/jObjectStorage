82815a9f5f25fac744420f659fd4db1c3a6223e4
Bart <bartpetka@gmail.com>
2018-11-16 21:28:23 +0100

    change main for testPurposes

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/App.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/App.java
@@ -11,19 +11,21 @@ public class App {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
         String host = scanner.nextLine();
+        String macbook = "192.168.10.55";
+        String local = "localhost";
 
         if(host.equals("server")) {
             MTServer mtServer = new MTServer(8080);
             mtServer.run();
         } else if (host.equals("client")) {
 
-            RemoteClient remoteClient1 = new RemoteClient("192.168.10.55", 8080);
-            RemoteClient remoteClient2 = new RemoteClient("192.168.10.55", 8080);
+            RemoteClient remoteClient1 = new RemoteClient(local, 8080);
+            RemoteClient remoteClient2 = new RemoteClient(local, 8080);
             String string = "POKA POKA";
             remoteClient1.addObject("bart", string);
             System.out.println(remoteClient2.getObject("bart"));
             System.out.println(remoteClient2.removeObject("bart"));
-            System.out.println(remoteClient2.removeObject("bart"));;
+            System.out.println(remoteClient2.removeObject("bart"));
         }
     }
 }
