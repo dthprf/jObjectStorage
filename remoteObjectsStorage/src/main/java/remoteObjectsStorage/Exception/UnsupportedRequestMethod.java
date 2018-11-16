c145ae8b0a8e5c202cf5e60f1f16bac8894fc54e
Bart <bartpetka@gmail.com>
2018-11-16 21:10:06 +0100

    Implement Custom exception for not supported methods

---
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Exception/UnsupportedRequestMethod.java
@@ -0,0 +1,7 @@
+package remoteObjectsStorage.Exception;
+
+public class UnsupportedRequestMethod extends Exception {
+    public UnsupportedRequestMethod(){}
+    public UnsupportedRequestMethod(String message) {super(message); }
+
+}
