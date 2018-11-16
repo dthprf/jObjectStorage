ef8eee0a037f42391f39b4d5d7afeac5f0796e11
Bart <bartpetka@gmail.com>
2018-11-16 21:08:51 +0100

    Implement constant with available methods

---
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Constant/MethodType.java
@@ -0,0 +1,7 @@
+package remoteObjectsStorage.Constant;
+
+public class MethodType {
+    public static final String PUT_METHOD = "PUT";
+    public static final String GET_METHOD = "GET";
+    public static final String DELETE_METHOD = "DELETE";
+}
