ef114d26ee1c681b3f9409a1d8e7277686af8347
Bart <bartpetka@gmail.com>
2018-11-17 14:22:58 +0100

    Rename class

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/Driver/ROSExceptionWrapper.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Driver/ROSExceptionWrapper.java
@@ -2,7 +2,7 @@ package remoteObjectsStorage.Driver;
 
 import remoteObjectsStorage.Exception.RemoteObjectStoreConnectionException;
 
-public class RemoteObjectStoreWrapper implements IConnectionExceptionHandler {
+public class ROSExceptionWrapper implements IConnectionExceptionHandler {
     @Override
     public void handle(Exception e, String errorMessage) {
         throw new RemoteObjectStoreConnectionException(errorMessage, e);
