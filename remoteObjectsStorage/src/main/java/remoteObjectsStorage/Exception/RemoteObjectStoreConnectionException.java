7fde15aafb40c83a6ee04b0894c2de19ff18b5c2
Bart <bartpetka@gmail.com>
2018-11-18 12:56:16 +0100

    Change type of connection exceptions

--- remoteObjectsStorage/src/main/java/remoteObjectsStorage/Exception/RemoteObjectStoreConnectionException.java
+++ remoteObjectsStorage/src/main/java/remoteObjectsStorage/Exception/RemoteObjectStoreConnectionException.java
@@ -1,6 +1,6 @@
 package remoteObjectsStorage.Exception;
 
-public class RemoteObjectStoreConnectionException extends Throwable {
+public class RemoteObjectStoreConnectionException extends RuntimeException {
     public RemoteObjectStoreConnectionException() {};
     public RemoteObjectStoreConnectionException(String message) {super(message);}
     public RemoteObjectStoreConnectionException(String message, Throwable t) {super(message, t);}
